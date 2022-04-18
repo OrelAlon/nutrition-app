150318d7579cdd64ff273a11470f71c1740ddd43
OrelAlon <orela231089@gmail.com>
2022-04-18 16:57:52 +0300

    fixed api key

--- src/api/ApiFatch.js
+++ src/api/ApiFatch.js
@@ -1,29 +1,18 @@
 import axios from "axios";
 
 export async function fetchFoodSearch(searchFood) {
-  const API_ID = "a9080630";
-  const API_KEY = "016eaef3bd150a51f2c3ff54fe5b5ccd";
-  // const API_ID = process.env.REACT_APP_API_ID;
-  // const API_KEY = process.env.REACT_APP_API_KEY;
-  // console.log(API_ID);
-  // console.log(API_KEY);
   const fixSpaceStr = searchFood.replace(/\s+/g, "%20");
 
   const { data } = await axios.get(
-    `https://api.edamam.com/api/food-database/v2/parser?ingr=${fixSpaceStr}&app_id=${API_ID}&app_key=${API_KEY}`
+    `https://api.edamam.com/api/food-database/v2/parser?ingr=${fixSpaceStr}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`
   );
+  // console.log(data);
   return data;
 }
 
 export async function fetchRecipeSearch(recipeSearch) {
-  const APP_ID = "32fb80e5";
-  const APP_KEY = "5c11e1a66466f2dcfda69eddc5d113ef";
-  // const query = 'pizza';
-  // const fixSpaceStr = searchFood.replace(/\s+/g, '%20');
-
   const data = await axios.get(
-    `https://api.edamam.com/search?q=${recipeSearch}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    `https://api.edamam.com/search?q=${recipeSearch}&app_id=${process.env.REACT_APP_RECIPE_API_ID}&app_key=${process.env.REACT_APP_RECIPE_API_KEY}`
   );
-  // console.log(data);
   return data;
 }
