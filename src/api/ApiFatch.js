5fb38d38f6ceff7a38fefe7ad5cb1fdcd5ca2f37
OrelAlon <orela231089@gmail.com>
2022-04-18 17:48:24 +0300

    test api key

--- src/api/ApiFatch.js
+++ src/api/ApiFatch.js
@@ -11,8 +11,11 @@ export async function fetchFoodSearch(searchFood) {
 }
 
 export async function fetchRecipeSearch(recipeSearch) {
+  const REACT_APP_RECIPE_API_ID = process.env.REACT_APP_RECIPE_API_ID;
+  const REACT_APP_RECIPE_API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
+
   const data = await axios.get(
-    `https://api.edamam.com/search?q=${recipeSearch}&app_id=${process.env.REACT_APP_RECIPE_API_ID}&app_key=${process.env.REACT_APP_RECIPE_API_KEY}`
+    `https://api.edamam.com/search?q=${recipeSearch}&app_id=${REACT_APP_RECIPE_API_ID}&app_key=${REACT_APP_RECIPE_API_KEY}`
   );
   return data;
 }
